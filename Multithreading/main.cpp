1c271a8b3f5ed85c2526089347a20cc0ab8d2ace
Bloemerang <enyalie.is@gmail.com>
2015-04-28 00:40:56 -0500

    Initial Commit

---
+++ Multithreading/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Multithreading
+//
+//  Created by Steven Bloemer on 4/28/15.
+//  Copyright © 2015 Steven Bloemer. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
